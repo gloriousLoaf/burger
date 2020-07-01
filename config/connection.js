bdbb818be0f6f022235ab9618f2a5ce6965095df
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-07-01 14:28:42 -0700

    Added JawsDB hook to connection.js.

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@
 const mysql = require(`mysql`);
 
 // DB Connection
-const connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: `localhost`,
     port: 3306,
     user: `root`,
     password: `root`,
     database: `burgers_db`
 });
+}
 connection.connect((err) => {
     if (err) throw err;
     console.log(`Connected as ID ${connection.threadId}`);
